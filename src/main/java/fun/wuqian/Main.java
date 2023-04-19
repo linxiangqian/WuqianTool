b9818fe1964077d50e8a3cf9510d3d58924dfd39
悟纤 <412887952@qq.com>
2023-04-19 21:23:50 +0800

    add

---
+++ src/main/java/fun/wuqian/Main.java
@@ -0,0 +1,11 @@
+package fun.wuqian;
+
+/**
+ * author：悟纤「公众号SpringBoot」
+ * date：${DATE}
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
